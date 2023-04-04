657ffbfc9ca6f3f5e8cd5cb2637034ba3d3e8888
Nasiebande <nikitaesther401@gmail.com>
2023-04-04 03:25:44 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,7 +8,7 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
@@ -17,5 +17,5 @@ size_t print_listint(const listint_t *h)
 		count++;
 	}
 
-	return (num);
+	return (count);
 }
