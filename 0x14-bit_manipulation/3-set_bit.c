f674b113bb6c9499127b174278fb48590eccfd37
Nasiebande <nikitaesther401@gmail.com>
2023-04-06 13:30:17 -0700

    3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the value of a bit at a given index
+ * @n: the unsigned long integer to get the bit from
+ * @index: the index of the bit to get, starting from 0
+ *
+ * Return: the value of the bit at index or -1 if an error occurred
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	unsigned long int mask = 1UL << index;
+
+	if ((n & mask) == 0)
+		return (0);
+	else
+		return (1);
+}
